b14bbe1c9614c84ebb7722077a7e88b0f7dbba9a
shailaja <donthulashailaja@gmail.com>
2024-09-04 11:17:13 +0530

    DC module

--- JRTP701-DataCollectionService/src/main/java/com/w3softtech/repository/IDcCaseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.w3softtech.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.w3softtech.entity.DcCaseEntity;
-
-public interface IDcCaseRepository extends JpaRepository<DcCaseEntity, Integer> {
-}
